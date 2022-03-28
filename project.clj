53fb98531d5a1f45dea042816ccf7f4117151c6c
Walter CI <piotr-yuxuan@users.noreply.github.com>
2022-03-28 06:46:13 +0000

    Update versions

--- project.clj
+++ project.clj
@@ -14,8 +14,8 @@
   :aot :all
   :profiles {:dev {:jvm-opts ["-Dclojure.compiler.disable-locals-clearing=true"]}
              :test {:dependencies [[com.bakdata.fluent-kafka-streams-tests/schema-registry-mock "2.5.3"]
-                                   [org.apache.kafka/kafka-clients "6.1.1-ce"]
-                                   [org.apache.kafka/kafka-streams-test-utils "6.1.1-ce"]]}
+                                   [org.apache.kafka/kafka-clients "7.0.1-ce"]
+                                   [org.apache.kafka/kafka-streams-test-utils "7.0.1-ce"]]}
              :jar {:jvm-opts ["-Dclojure.compiler.disable-locals-clearing=false"
                               "-Dclojure.compiler.direct-linking=true"]}
              :provided {:dependencies [[org.apache.avro/avro "1.11.0"]
