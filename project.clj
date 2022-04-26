168328e5e80f5b6ebf2477a399d1a2f8546efadb
Walter CI <piotr-yuxuan@users.noreply.github.com>
2022-04-26 18:44:51 +0000

    Update dependency versions

--- project.clj
+++ project.clj
@@ -21,7 +21,7 @@
                               "-Dclojure.compiler.direct-linking=true"]}
              :provided {:dependencies [[org.apache.avro/avro "1.11.0"]
                                        [org.clojure/clojure "1.11.1"]]}
-             :kaocha [:test {:dependencies [[lambdaisland/kaocha "1.65.1029"]]}]}
+             :kaocha [:test {:dependencies [[lambdaisland/kaocha "1.66.1034"]]}]}
   :repositories [["confluent" {:url "https://packages.confluent.io/maven/"}]]
   :deploy-repositories [["clojars" {:sign-releases false
                                     :url "https://clojars.org/repo"
