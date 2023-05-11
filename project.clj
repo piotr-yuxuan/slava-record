1112f89b609c176ed191a2f9c5660f032fb0a4de
Walter CI <piotr-yuxuan@users.noreply.github.com>
2023-05-11 18:30:25 +0000

    Update dependency versions

--- project.clj
+++ project.clj
@@ -14,7 +14,7 @@
   :global-vars {*warn-on-reflection* true}
   :aot :all
   :profiles {:dev {:jvm-opts ["-Dclojure.compiler.disable-locals-clearing=true"]}
-             :test {:dependencies [[com.bakdata.fluent-kafka-streams-tests/schema-registry-mock "2.8.1"]
+             :test {:dependencies [[com.bakdata.fluent-kafka-streams-tests/schema-registry-mock "2.8.2"]
                                    [org.apache.kafka/kafka-clients "7.4.0-ce"]
                                    [org.apache.kafka/kafka-streams-test-utils "7.4.0-ce"]]}
              :jar {:jvm-opts ["-Dclojure.compiler.disable-locals-clearing=false"
