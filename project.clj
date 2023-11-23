47640fd930d20f640898a8370360041e89c756b5
Walter CI <piotr-yuxuan@users.noreply.github.com>
2023-11-23 18:31:01 +0000

    Update dependency versions

--- project.clj
+++ project.clj
@@ -10,7 +10,7 @@
   :pom-addition [:developers [:developer
                               [:name "胡雨軒 Петр"]
                               [:url "https://github.com/piotr-yuxuan"]]]
-  :dependencies [[potemkin/potemkin "0.4.6"]]
+  :dependencies [[potemkin/potemkin "0.4.7"]]
   :global-vars {*warn-on-reflection* true}
   :aot :all
   :profiles {:dev {:jvm-opts ["-Dclojure.compiler.disable-locals-clearing=true"]}
